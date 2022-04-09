9b2069d3d9bf58887132ab3c7100d6985a77f4c0
carolyn-manning <carolyn.manning3@gmail.com>
2022-04-09 14:58:48 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
-import counterReducer from "./reducers/counterReducer";
+import { Provider } from "react-redux"; /* code change */
+import counterReducer from "./reducers/counterReducer.js";
 import App from "./App";
 import "./index.css";
 
 const store = createStore(
   counterReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-); /* code change */
+); 
 
 ReactDOM.render(
-  <App store={store} />,
+  <Provider store={store}>
+    <App />
+  </Provider> /* code change */,
   document.getElementById("root")
 );
